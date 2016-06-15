84b3da497c56912ff7e3c59411a5d0f380207549
Jordan Cameron Uehara <rakunko@users.noreply.github.com>
2016-06-15 10:53:05 -0400

    Update mdraidchecker.sh

--- mdraidchecker.sh
+++ mdraidchecker.sh
@@ -18,8 +18,8 @@ generate_raidlist () {
 cleanup
 touch /tmp/raid
 touch /tmp/failed_raid
-cat /proc/mdstat | grep md[[:digit:]] | awk '{ print "\/dev\/" $1}' 2> /dev/null 1>> /tmp/raid
-grep -B1 _ /proc/mdstat | grep md[[:digit::]] | awk '{ print "\/dev\/" $1}' 2> /dev/null 1>> /tmp/failed_raid
+cat /proc/mdstat | grep md[0-9]* | awk '{ print "\/dev\/" $1}' 2> /dev/null 1>> /tmp/raid
+grep -B1 _ /proc/mdstat | grep md[0-9]* | awk '{ print "\/dev\/" $1}' 2> /dev/null 1>> /tmp/failed_raid
 }
 
 # performs main reports to the logs
@@ -34,20 +34,20 @@ if  grep -B1 -q _ /proc/mdstat ; then
 		mdadm --detail $line >> $LOG
 	done < /tmp/failed_raid
 else
+        echo $TIMESTAMP >> $LOG
 	echo "RAID HEALTHY" | tee -a $LOG
-	echo $TIMESTAMP >> $LOG
 	cat /proc/mdstat >> $LOG
 fi
 }
 
 # Manually checks raids
 perform_check () {
-echo "Displaying RAID stats please proceed with 'q': screen (1)/3." | less && cat /proc/mdstat | less && mdadm --detail /dev/md* | less
+echo "Displaying RAID stats, please proceed with 'q': screen (1)/3." | less && cat /proc/mdstat | less && mdadm --detail /dev/md* | less
 }
 
 # Allows easy navigation of logs
 perform_log () {
-less $LOG
+echo "Displaying Logs, please proceed with 'q': screen (1)/2." | less && less $LOG
 }
 
 # CASE Menu
